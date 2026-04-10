4222010042d5ffba36f84d85917fcc41d6736133
Rahul <rahulsaha6917@gmail.com>
2026-04-11 00:46:08 +0530

    Print DB URL from environment variable
    Added a line to print the database URL from environment variables.

--- src/main/java/com/example/demo/OnlineBankingBackendApplication.java
+++ src/main/java/com/example/demo/OnlineBankingBackendApplication.java
@@ -1,9 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class OnlineBankingBackendApplication {
 
     public static void main(String[] args) {
 
-        // 🔥 Add this line
         System.out.println("DB URL: " + System.getenv("SPRING_DATASOURCE_URL"));
 
         SpringApplication.run(OnlineBankingBackendApplication.class, args);
